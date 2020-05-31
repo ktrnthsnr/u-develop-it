440423c5bd1bf35bc46e7c70abe3aafdf5ee1861
ktrnthsnr <asakthisner@hotmail.com>
2020-05-31 02:36:27 -0700

    connected to db, added a select ALL candidates, wrapped in an Express.js get route

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+//-- Usage: to run this script, enter in the terminal
+// -- $ npm start 
+// -- Then open a browser to view  http://localhost:3003/api/candidates (not https!)
+
+// --------  requires, PORT, and middleware  --------- //
+
 // -- import express module
 const express = require('express');
 // -- import the sqlite3 module
@@ -11,7 +17,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
+// --------  connect to database  --------- //
+
 const db = new sqlite3.Database('./db/election.db', err => {
     if (err) {
       return console.error(err.message);
@@ -19,10 +26,10 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
   });
 
+// --------  routes --------- //
 
-// -- functions
-
-// -- test the PORT connection //-- test by running $ npm start & http://localhost:3003 (not https!)
+// -- test the PORT connection 
+  // -- To view 'Hello World', open a browser to view  http://localhost:3003 (not https!)
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
@@ -30,27 +37,78 @@ app.get('/', (req, res) => {
   });
 
 
-// -- test the db connection
-  db.all(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// -- Get all candidates, wrapped in an Express.js route
+   // --- test: 1) restart server $ npm start 2) open browser http://localhost:3001/api/candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
   });
 
-//--place after all GET
+
+//--place 404 catch all   *after*   all GET
 //  -- Default response for any other request(Not Found) Catch all  
 app.use((req, res) => {
     res.status(404).end();
   });
 
+// --------  functions --------- //
+
+// Create a candidate
+        // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+        //             VALUES (?,?,?,?)`;
+        // const params = [1, 'Ronald', 'Firbank', 1];
+        // // ES5 function, not arrow function, to use this
+        // db.run(sql, params, function(err, result) {
+        // if (err) {
+        //     console.log(err);
+        // }
+        // console.log(result, this.lastID);
+        // });
+
+// -- test the db connection, get ALL candidates - simple
+            // db.all(`SELECT * FROM candidates`, (err, rows) => {
+            //     console.log(rows);
+            // });
 
 
-// -- Start server after DB connection,
-    // -- wrapping the Express.js server in the db connection, in event handler.
+// GET a single candidate
+            // db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+            //     if(err) {
+            //       console.log(err);
+            //     }
+            //     console.log(row);
+            //   });
+ 
+            
+// Delete a candidate
+        // db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+        //     if (err) {
+        //       console.log(err);
+        //     }
+        //     console.log(result, this, this.changes);
+        //   });
+
+
+
+// --------  event handler --------- //
+
+// -- Listen / start server after DB connection
+//   -- wrapping the Express.js server in the db connection, in event handler.
 db.on('open', () => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   });
-
 
 
 // // --- previous listen
